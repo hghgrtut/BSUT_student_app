43fc4046e349b3f7320a2f09dd9e14c931860220
Hg Hg <hghghghg.hh39@gmail.com>
2020-09-21 22:46:19 +0300

    release of MVP and CI

---
+++ app/src/main/java/by/bsut/studapp/constants/Urls.kt
@@ -0,0 +1,8 @@
+package by.bsut.studapp.constants
+
+const val BSUT_URL = "https://bsut.by"
+const val ENDPOINT_OBSCH_ORG = "/contacts/all/obsh-organiz"
+const val ENDPOINT_REKTOR_QUESTION = "/contacts/electronic-appeal-rector"
+const val ENDPOINT_TIMETABLE = "/timetable"
+
+const val DIST_URL = "http://dist.bsut.by/"
--- app/src/main/java/by/bsut/studapp/ui/main/MainFragment.kt
+++ app/src/main/java/by/bsut/studapp/ui/main/MainFragment.kt
@@ -1,11 +1,18 @@
 package by.bsut.studapp.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.bsut.studapp.R
+import by.bsut.studapp.constants.BSUT_URL
+import by.bsut.studapp.constants.DIST_URL
+import by.bsut.studapp.constants.ENDPOINT_OBSCH_ORG
+import by.bsut.studapp.constants.ENDPOINT_REKTOR_QUESTION
+import by.bsut.studapp.constants.ENDPOINT_TIMETABLE
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     private val navController by lazy(LazyThreadSafetyMode.NONE) { findNavController() }
@@ -15,6 +22,34 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
         view.findViewById<Button>(R.id.go_to_auditories).setOnClickListener {
             navController.navigate(R.id.action_mainFragment_to_auditoriesFragment)
+        }
+
+        view.findViewById<Button>(R.id.go_to_dist).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(DIST_URL) }
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.go_to_obsch_org).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(BSUT_URL + ENDPOINT_OBSCH_ORG) }
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.go_to_rektor).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(BSUT_URL + ENDPOINT_REKTOR_QUESTION) }
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.go_to_site).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(BSUT_URL) }
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.go_to_timetable).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(BSUT_URL + ENDPOINT_TIMETABLE) }
+            startActivity(intent)
         }
     }
 }
